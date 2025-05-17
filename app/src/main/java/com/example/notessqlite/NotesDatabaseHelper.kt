da1684c183d7e7ae45386a74e899b55c74793ed0
Hapi-W <spendasbjm@gmail.com>
2025-05-17 23:27:47 +0800

    Initial Commit

--- app/src/main/java/com/example/notessqlite/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/notessqlite/NotesDatabaseHelper.kt
@@ -10,13 +10,13 @@ class NotesDatabaseHelper (contex: Context) :SQLiteOpenHelper(contex, DATABASE_N
         private const val DATABASE_NAME = "notesapp.db"
         private const val DATABASE_VERSION = 1
         private const val TABLE_NAME = "allnotes"
-        private const val COULMN_ID = "id"
-        private const val COULMN_TITLE = "title"
-        private const val COULMN_CONTENT = "content"
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_TITLE = "title"
+        private const val COLUMN_CONTENT = "content"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val createTableQuery = "CREATE TABLE $TABLE_NAME($COULMN_ID INTEGER PRIMARY KEY, $COULMN_TITLE TEXT, $COULMN_CONTENT TEXT)"
+        val createTableQuery = "CREATE TABLE $TABLE_NAME($COLUMN_ID INTEGER PRIMARY KEY, $COLUMN_TITLE TEXT, $COLUMN_CONTENT TEXT)"
         db?.execSQL(createTableQuery)
     }
 
@@ -28,10 +28,29 @@ class NotesDatabaseHelper (contex: Context) :SQLiteOpenHelper(contex, DATABASE_N
     fun  insertNote(note: Note){
         val db = writableDatabase
         val values = ContentValues().apply {
-            put(COULMN_TITLE, note.title)
-            put(COULMN_CONTENT, note.content)
+            put(COLUMN_TITLE, note.title)
+            put(COLUMN_CONTENT, note.content)
         }
         db.insert(TABLE_NAME, null , values)
         db.close()
+    }
+
+    fun getAllNotes(): List<Note>{
+        val notesList = mutableListOf<Note>()
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(query, null)
+
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+            val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+            val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+            val note = Note(id,title,content)
+            notesList.add(note)
+        }
+        cursor.close()
+        db.close()
+        return notesList
     }
 }
