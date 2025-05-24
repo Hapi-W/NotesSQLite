0473a42962f1d3f5ba7fcf6898155f6b4b9f0b2c
Hapi-W <spendasbjm@gmail.com>
2025-05-24 20:18:39 +0800

    Initial Commit

--- app/src/main/java/com/example/notessqlite/NotesAdapter.kt
+++ app/src/main/java/com/example/notessqlite/NotesAdapter.kt
@@ -1,13 +1,13 @@
 package com.example.notessqlite
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 class NotesAdapter( private var notes: List<Note>, context: Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
     class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
@@ -32,6 +32,13 @@ class NotesAdapter( private var notes: List<Note>, context: Context) : RecyclerV
         val note = notes[position]
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
+
+        holder.updateButton.setOnClickListener {
+            val intent = Intent(holder.itemView.context, UpdateNoteActivity::class.java).apply {
+                putExtra("note_id",note.id)
+            }
+            holder.itemView.context.startActivity(intent)
+        }
     }
     fun refreshData(newNotes : List<Note>){
         notes = newNotes
